59da9dcf5dcd641518a4591515b31ab2d8430503
bushikot <bushikot@gmail.com>
2018-10-20 23:39:52 +0300

    Stubs file extended with msgpack extension classes

--- msgpack.php
+++ msgpack.php
@@ -1,12 +1,79 @@
 <?php
-/**
- * @param mixed $data
- * @return mixed
- */
-function msgpack_pack($data){}
 
 /**
- * @param string $data
+ * @param mixed $value
  * @return string
  */
-function msgpack_unpack($data){}
+function msgpack_pack($value) {}
+
+/**
+ * @param string $str
+ * @return object|string $object 
+ * @return mixed
+ */
+function msgpack_unpack($str, $object = null) {}
+
+class MessagePack {
+    const OPT_PHPONLY = -1001;
+
+    /**
+     * @param bool $opt
+     */
+    public function __construct($opt = null) {}
+
+    /**
+     * @param int $option
+     * @param mixed $value
+     */
+    public function setOption($option, $value) {}
+
+    /**
+     * @param mixed $value
+     * @return string
+     */
+    public function pack($value) {}
+
+    /**
+     * @param string $str
+     * @return object|string $object 
+     */
+    public function unpack($str, $object = null) {}
+
+    /**
+     * @return MessagePackUnpacker
+     */
+    public function unpacker() {}
+}
+
+class MessagePackUnpacker {
+
+    /**
+     * @param bool $opt
+     */
+    public function __construct($opt = null) {}
+
+    /**
+     * @param int $option
+     * @param mixed $value
+     */
+    public function setOption($option, $value) {}
+
+    /**
+     * @param string $str
+     */
+    public function feed($str) {}
+
+    /**
+     * @param string $str
+     * @param int $offset
+     */
+    public function execute($str, $offset) {} 
+
+    /**
+     * @param object|string $object
+     * @return mixed
+     */
+    public function data($object = null) {}
+
+    public function reset() {}
+}
