12bb5ad19772685de38641e8fc40b164ada88b50
Maciej Kłak <klak.maciej@gmail.com>
2018-10-28 16:50:36 +0100

    CS fix

--- msgpack.php
+++ msgpack.php
@@ -4,14 +4,14 @@
  * @param mixed $value
  * @return string
  */
-function msgpack_pack($value) {}
+function msgpack_pack ($value) {}
 
 /**
  * @param string $str
- * @return object|string $object 
+ * @return object|string $object
  * @return mixed
  */
-function msgpack_unpack($str, $object = null) {}
+function msgpack_unpack ($str, $object = null) {}
 
 class MessagePack {
     const OPT_PHPONLY = -1001;
@@ -19,61 +19,61 @@ class MessagePack {
     /**
      * @param bool $opt
      */
-    public function __construct($opt = null) {}
+    public function __construct ($opt = null) {}
 
     /**
      * @param int $option
      * @param mixed $value
      */
-    public function setOption($option, $value) {}
+    public function setOption ($option, $value) {}
 
     /**
      * @param mixed $value
      * @return string
      */
-    public function pack($value) {}
+    public function pack ($value) {}
 
     /**
      * @param string $str
      * @return object|string $object 
      */
-    public function unpack($str, $object = null) {}
+    public function unpack ($str, $object = null) {}
 
     /**
      * @return MessagePackUnpacker
      */
-    public function unpacker() {}
+    public function unpacker () {}
 }
 
 class MessagePackUnpacker {
-
     /**
      * @param bool $opt
      */
-    public function __construct($opt = null) {}
+    public function __construct ($opt = null) {}
 
     /**
      * @param int $option
      * @param mixed $value
      */
-    public function setOption($option, $value) {}
+    public function setOption ($option, $value) {}
 
     /**
      * @param string $str
      */
-    public function feed($str) {}
+    public function feed ($str) {}
 
     /**
      * @param string $str
      * @param int $offset
      */
-    public function execute($str, $offset) {} 
+    public function execute ($str, $offset) {}
 
     /**
      * @param object|string $object
+     *
      * @return mixed
      */
-    public function data($object = null) {}
+    public function data ($object = null) {}
 
-    public function reset() {}
+    public function reset () {}
 }
